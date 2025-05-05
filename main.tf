a512f09fee66a8b9130532e4a73844d9b9465205
Azure6363 <azure.devops6363@gmail.com>
2025-05-05 18:23:39 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = var.resource_group_name
+  location = var.location
+}
---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+resource_group_name = "devops-rg"
+location            = "East US"
